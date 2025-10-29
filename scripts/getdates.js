b159d50abb7b4f3c23fa05d9d5d23617cbe830a8
unknown <robbie2497@gmail.com>
2025-10-28 23:39:45 -0600

    Getting started on index.html

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+const currentYear = document.querySelector('#currentYear');
+const lastModified = document.querySelector('#lastModified');
+
+currentYear.innerHTML = today.getFullYear();
+lastModified.innerHTML = document.lastModified;
